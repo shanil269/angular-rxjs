42199b025aff380c196cf1d9b83961b5ae6436c7
Md Muhaimin Hossain <shanilhossain2609@gmail.com>
2023-06-15 19:37:03 +0900

    added query params logic in getForecast function

--- rxjswithangular/src/app/weather/forecast.service.ts
+++ rxjswithangular/src/app/weather/forecast.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpParams } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +9,19 @@ import { Observable } from 'rxjs';
 export class ForecastService {
 
   constructor() { }
+
+  getForecast() {
+    return this.getCurrentLocation()
+      .pipe(
+        map(coords => {
+          return new HttpParams()
+            .set('lat', coords.latitude)
+            .set('lon', coords.longitude)
+            .set('appid', '1d8aaf55e6c3d2604e2485fd2296570e')
+            .set('units', 'metric')
+        })
+      )
+  }
 
   getCurrentLocation() {
     return new Observable<GeolocationCoordinates>((observer) => {
