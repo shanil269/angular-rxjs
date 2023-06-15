48e888b89b33dac68a756574713fe9d30212efe4
Md Muhaimin Hossain <shanilhossain2609@gmail.com>
2023-06-15 21:00:54 +0900

    added switchmap observable for getting the weather response

--- rxjswithangular/src/app/weather/forecast.service.ts
+++ rxjswithangular/src/app/weather/forecast.service.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@angular/core';
-import { HttpParams } from '@angular/common/http'
+import { HttpParams, HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators'
+import { map, switchMap } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForecastService {
+  // private url = 'http://api.weatherapi.com/v1/forecast.json?'
+  private url = 'https://api.openweathermap.org/data/2.5/forecast'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getForecast() {
     return this.getCurrentLocation()
       .pipe(
         map(coords => {
+          console.log(coords.latitude)
+          console.log(coords.longitude)
           return new HttpParams()
             .set('lat', coords.latitude)
             .set('lon', coords.longitude)
-            .set('appid', '1d8aaf55e6c3d2604e2485fd2296570e')
-            .set('units', 'metric')
-        })
+            .set('key', '1d8aaf55e6c3d2604e2485fd2296570e')
+          // .set('q', coords.latitude).append('q', coords.longitude)
+
+          // .set('units', 'metric')
+        }),
+        switchMap(params => this.http.get(this.url, { params }))
       )
   }
 
