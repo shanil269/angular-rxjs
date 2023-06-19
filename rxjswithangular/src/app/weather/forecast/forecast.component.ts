9cbb483091a02f8d4a370e07c3eee806a28783fc
Md Muhaimin Hossain <shanilhossain2609@gmail.com>
2023-06-19 16:02:33 +0900

    got the response data in template

--- rxjswithangular/src/app/weather/forecast/forecast.component.ts
+++ rxjswithangular/src/app/weather/forecast/forecast.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ForecastService } from '../forecast.service';
 
+interface ForecastData {
+  dateString: string,
+  temp: number
+}
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
   styleUrls: ['./forecast.component.css']
 })
 export class ForecastComponent implements OnInit {
+  forecastData: ForecastData[] = []
 
   constructor(private forecastService: ForecastService) {
     forecastService.getForecast()
-      .subscribe((weatherResponse) => {
-        console.log(weatherResponse);
-
+      .subscribe((forecastData) => {
+        this.forecastData = forecastData
       })
   }
 
